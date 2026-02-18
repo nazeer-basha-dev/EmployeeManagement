d1becb5464ba2c4ed6a31ddce71fa1523e1268f8
Nazeer <nazeer0075@outlook.com>
2026-02-18 11:54:46 +0530

    Implemented versioning

--- EmployeeManagementAPI/EmployeeManagementAPI/Program.cs
+++ EmployeeManagementAPI/EmployeeManagementAPI/Program.cs
@@ -1,8 +1,31 @@
+using Asp.Versioning;
+using Asp.Versioning.Conventions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddApiVersioning(options =>
+{
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.DefaultApiVersion = new Asp.Versioning.ApiVersion(1, 0);
+    options.ReportApiVersions = true;
+    options.ApiVersionReader = ApiVersionReader.Combine(
+        new QueryStringApiVersionReader("api-version"),
+        new HeaderApiVersionReader("x-api-version"),
+        new MediaTypeApiVersionReader("api-version")
+        );
+}).AddMvc(options =>
+{
+    options.Conventions.Add(new VersionByNamespaceConvention());
+}).AddApiExplorer(options =>
+{
+    options.GroupNameFormat = "'v'V";
+    options.SubstituteApiVersionInUrl = true;
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
